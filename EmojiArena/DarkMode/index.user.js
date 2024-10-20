7f0a1143fd98e0503134840103fdeee5ee861261
Roman Anishchenko <121314722+GameRoMan@users.noreply.github.com>
2024-10-20 19:38:30 +0100

    Update index.user.js

--- EmojiArena/DarkMode/index.user.js
+++ EmojiArena/DarkMode/index.user.js
@@ -5,7 +5,7 @@
 // @match           https://g.wildwest.gg/g/d893cd5b-68e2-46a4-a23d-9efb65a198dd*
 // @match           https://g.wildwest.gg/g/beccb87f-c9ae-4cd6-bdc2-156e56a82da9*
 // @grant           none
-// @version         1.1
+// @version         1.2
 // @author          GameRoMan
 // @description     Dark Mode for Emoji Arena
 // @downloadURL     https://github.com/GameRoMan/wildwest/raw/refs/heads/main/EmojiArena/DarkMode/index.user.js
@@ -19,6 +19,8 @@
         document.body.style.color = '#666666';
 
         document.getElementById('game-container').style.backgroundColor = '#111111';
+
+        document.getElementById('popup').style.backgroundColor = '#111111';
 
         document.querySelectorAll('.page').forEach(element => {
             element.style.backgroundColor = '#222222';
@@ -61,8 +63,16 @@
             element.style.backgroundColor = '#111111';
         });
 
+        document.querySelectorAll('.guide-emoji').forEach(element => {
+            element.style.backgroundColor = '#222222';
+        });
+
         document.querySelectorAll('.achievement-row').forEach(element => {
             element.style.backgroundColor = '#111111';
+        });
+
+        document.querySelectorAll('.achievement-emoji').forEach(element => {
+            element.style.backgroundColor = '#222222';
         });
     }
 
